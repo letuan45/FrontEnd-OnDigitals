d1922c7d120fb7fdd48afa2ad32c828109a025c6
Tuấn Lê <n19dccn177@student.ptithcm.edu.vn>
2023-08-21 12:48:35 +0700

    update-base

---
+++ src/components/MainHead/MainHead.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainHead = () => {
+  return (
+    <div>MainHead</div>
+  )
+}
+
+export default MainHead
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import MainHead from '@/components/MainHead/MainHead';
+import Layout from '@/components/layout/Layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MainHead />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
